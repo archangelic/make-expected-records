0c37d44fa2426c3200d19e22c961413233468160
Mal Hancock <mallory@archangelic.space>
2023-07-05 16:05:32 -0700

    add options to script

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 # make-expected-records
 Takes output of `python main.py read ...` and makes an `expected_records.jsonl` for airbyte connectors
 
-Only outputs up to 3 records per stream.
+Requires click (`pip install click`)
 
+The usage instructions can be found using `--help`.
+```
+Usage: make_expected_records.py [OPTIONS] RECORDS
+
+Options:
+  -s, --streams TEXT    Comma separated list of streams
+  -n, --number INTEGER  Number of records per stream  [default: 3]
+  -a, --all             Output all records for streams
+  --help                Show this message and exit.
+```
+
+Example usage:
 ```
 $ python main.py read --config secrets/config.json --catalog integration_tests/configured_catalog.json > expected_records.txt
 $ /path/to/make_expected_records.py expected_records.txt expected_records.jsonl
--- make_expected_records.py
+++ make_expected_records.py
@@ -2,36 +2,49 @@
 import json
 import sys
 
+import click
+
 # This script takes the output from `python main.py read ...` and puts into the format expected by expected_records.jsonl
 
-# give the output of the `main.py read ...`
-filename = sys.argv[1]
+def split_streams(ctx, param, value):
+    try:
+        return [s.strip() for s in value.split(',') if s]
+    except AttributeError:
+        return []
 
-# read the input
-with open(filename) as r:
-    raw_records = r.readlines()
+@click.command()
+@click.argument('records', type=click.File('rb'))
+@click.option('--streams', '-s', callback=split_streams, help="Comma separated list of streams")
+@click.option('--number', '-n', type=int, default=3, show_default=True, help="Number of records per stream")
+@click.option('--all', '-a', '_all',  is_flag=True, help='Output all records for streams')
+def parse_records(records, streams, records_per_stream, _all):
+    raw_records = records.readlines()
 
-# sort out only the records
-parsed_records = []
-stream_records = []
-for r in raw_records:
-    line = json.loads(r.strip())
-    if line['type'] == 'RECORD':
-        parsed_records.append(json.dumps(line['record']))
+    # sort out only the records
+    parsed_records = []
+    stream_records = []
+    for r in raw_records:
+        line = json.loads(r.strip())
+        if line['type'] == 'RECORD':
+            parsed_records.append(json.dumps(line['record']))
 
-# get a set of stream names from the records
-streams = set()
-for s in parsed_records:
-    streams.add(json.loads(s)['stream'])
+    # get a set of stream names from the records
+    streams_found = set()
+    for s in parsed_records:
+        streams_found.add(json.loads(s)['stream'])
 
-# select up to 3 records as examples
-for s in streams:
-    stream = [x for x in parsed_records if json.loads(x)['stream'] == s]
-    stream = stream[:3]
-    for x in stream:
-        stream_records.append(x)
+    # select up to 3 records as examples
+    for s in streams_found:
+        if s in streams or not streams:
+            stream = [x for x in parsed_records if json.loads(x)['stream'] == s]
+            if not _all:
+                stream = stream[:records_per_stream]
+            for x in stream:
+                stream_records.append(x)
 
-# print the output
-for r in stream_records:
-    print(r)
+    # print the output
+    for r in stream_records:
+        print(r)
 
+if __name__ == '__main__':
+    parse_records()
